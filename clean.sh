c6743b81595b808064348295fffc1cc8320d8cd2
Cooper <interruping@naver.com>
2020-02-09 16:53:58 +0900

    Add some basic settings and file.

---
+++ clean.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+rm cmake_install.cmake
+rm CMakeCache.txt
+rm -rf CMakeFiles/
+rm Makefile
+rm *.cmake
+rm *.tcl
+rm -rf Testing/
+
---
+++ include/csvpp/parser.hpp
@@ -0,0 +1,7 @@
+namespace csvpp {
+class parser
+{
+
+};
+} // end of class csvpp
+#include <csvpp/impl/parser.hpp>
